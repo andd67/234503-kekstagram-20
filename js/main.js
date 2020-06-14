0501b312bf0cf8cee716540b6ecac7b0f6a0f191
Андрей <itc-d-aa@yandex.ru>
2020-06-15 02:06:48 +0300

    Травис

--- js/main.js
+++ js/main.js
@@ -4,12 +4,12 @@ var NUMBER_FOTOS = 25; // количество фото
 var MIN_NUNBER_LIKES = 15; // минимальное число лайков
 var MAX_NUNBER_LIKES = 200; // максимальное число лайков
 var COMMENTS = [
-  '"Всё отлично!"',
-  '"В целом всё неплохо. Но не всё."',
-  '"Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально."',
-  '"Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше."',
-  '"Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше."',
-  '"Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!"'
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ]; // массив комментариев
 var NAMES = [
   'Игорь',
@@ -23,48 +23,54 @@ var NAMES = [
 ]; // массив имён
 var templatePicture = document.querySelector('#picture').content.querySelector('.picture'); // шаблон для создания DOM-элементов
 var blockPicture = document.querySelector('.pictures'); // блок для загрузки DOM-элементов
-var bigPicture = document.querySelector('.big-picture'); // section полноэкранного показа изображения
-var bigPictureImg = document.querySelector('.big-picture__img'); // блок просмотра большого изображения
 
-var createRandomNumberRange = function (min, max) {
+var getRandomNumberRange = function (min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }; // функция, генерирующая целое число из заданного диапозона
 
-var createComments = function () {
-  var comments = [];
-  if (Math.random() > 0.5) {
-    comments[0] = COMMENTS [createRandomNumberRange(0, COMMENTS.length - 1)] + ' Автор: ' + NAMES[createRandomNumberRange(0, NAMES.length - 1)] + '.';
-    comments[1] = COMMENTS [createRandomNumberRange(0, COMMENTS.length - 1)] + ' Автор: ' + NAMES[createRandomNumberRange(0, NAMES.length - 1)] + '.';
-  } else {
-    comments[0] = COMMENTS [createRandomNumberRange(0, COMMENTS.length - 1)] + ' Автор: ' + NAMES[createRandomNumberRange(0, NAMES.length - 1)] + '.';
-  }
-  return comments;
-}; // функция, создающая комментарии
+var createComment = function (comments, names) {
+  return {
+    avatar: 'img/avatar-' + getRandomNumberRange(1, 6) + '.svg',
+    message: comments[getRandomNumberRange(0, comments.length - 1)],
+    name: names[getRandomNumberRange(0, names.length - 1)]
+  };
+}; // функция, создающая один комментарий
 
-var createDescriptionFoto = function (number) {
+var createArrayComments = function () {
+  var arrayComments = [];
+  var arrayCommentsLength = getRandomNumberRange(1, 4);
+  for (var i = 0; i < arrayCommentsLength; i++) {
+    arrayComments.push(createComment(COMMENTS, NAMES));
+  }
+  return arrayComments;
+}; // функция, создающая массив комментов (от одного до 4ёх штук)
+
+var createObjectPhoto = function (number) {
   return {
     url: 'photos/' + (number + 1) + '.jpg',
-    likes: createRandomNumberRange(MAX_NUNBER_LIKES, MIN_NUNBER_LIKES),
-    comments: createComments()
+    description: 'descriptionPhoto',
+    likes: getRandomNumberRange(MIN_NUNBER_LIKES, MAX_NUNBER_LIKES),
+    comments: createArrayComments()
   };
-}; // функция, создающая один объект описания фото
+};// функция, создающая объект с описанием одной фотограффии
 
-var createArrayPhoto = function (number) {
+var generateArrayPhoto = function () {
   var arrayPhotos = [];
-  for (var i = 0; i < number; i++) {
-    arrayPhotos[i] = createDescriptionFoto(i);
+  for (var i = 0; i < 25; i++) {
+    arrayPhotos.push(createObjectPhoto(i));
   }
   return arrayPhotos;
-}; // функция, создающая массив с объектами описания фото
+}; // функция, генерирующая массив с объектами описария фото
 
+// =======================================================================================================================================
 
 var makePicture = function (picture) {
   var elPicture = templatePicture.cloneNode(true);
   elPicture.querySelector('.picture__img').src = picture.url;
-  elPicture.querySelector('.picture__comments').textContent = picture.comments;
   elPicture.querySelector('.picture__likes').textContent = picture.likes;
+  elPicture.querySelector('.picture__comments').textContent = picture.comments.length;
   return elPicture;
-}; // фуекция, создающая один DOM-элемент по шаблону
+}; // функция, создающая один DOM-элемент по шаблону
 
 var makeFragment = function (picture) {
   var fragment = document.createDocumentFragment();
@@ -74,18 +80,4 @@ var makeFragment = function (picture) {
   blockPicture.appendChild(fragment);
 }; // функция, создающая все DOM-элементы, записывает их все в документ фрагмент и помещающая их на страницу.
 
-var bigFoto = function (picture) {
-  bigPicture.classList.remove('hidden');
-  bigPictureImg.querySelector('img').src = picture.url;
-  bigPicture.querySelector('.likes-count').textContent = picture.likes;
-  bigPicture.querySelector('.comments-count').textContent = picture.comments.length;
-}; // функция, большое фото
-
-makeFragment(createArrayPhoto(NUMBER_FOTOS));
-
-bigFoto(createArrayPhoto(NUMBER_FOTOS)[3]);
-
-
-
-
-
+makeFragment(generateArrayPhoto());
